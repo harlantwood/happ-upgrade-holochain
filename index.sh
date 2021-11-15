20ed65bb8e9b5549d562caf1932119cbc34292a8
Harlan T Wood <code@harlantwood.net>
2021-11-14 16:17:48 -1000

    Shell script for bin

--- index.sh
+++ index.sh
@@ -3,4 +3,4 @@ set -euxo pipefail
 
 package_dir=${BASH_SOURCE[0]}/../..
 which node
-node --loader ${package_dir}/node_modules/ts-node/esm --experimental-specifier-resolution=node index.ts $*
+node --loader ${package_dir}/ts-node/esm --experimental-specifier-resolution=node index.ts $*
