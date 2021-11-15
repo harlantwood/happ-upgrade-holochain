6c215dddfbb9a4abe1c45bcef4aa7998d49b2187
Harlan T Wood <code@harlantwood.net>
2021-11-14 15:36:52 -1000

    Prettier

--- index.ts
+++ index.ts
@@ -1,14 +1,13 @@
-import { join, resolve } from "path"
-import { readFileSync, writeFileSync } from "fs"
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import glob from "glob"
+import { join, resolve } from 'path'
+import { readFileSync, writeFileSync } from 'fs'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import glob from 'glob'
 
 import { run } from './run'
 
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 // console.log({ __dirname })
 
 main()
@@ -21,8 +20,8 @@ function main() {
   // console.log({ version, happ_dir })
 
   if (!version) {
-    console.error("ERROR: Missing holochain version.  Sample usage:")
-    console.error("npx harlantwood/happ-upgrade-holochain 0.0.115")
+    console.error('ERROR: Missing holochain version.  Sample usage:')
+    console.error('npx harlantwood/happ-upgrade-holochain 0.0.115')
     process.exit(1)
   }
 
@@ -32,46 +31,59 @@ function main() {
   // update - hc - sha:
 
   console.log(`⚙️  Updating happ using hdk/holochain version: ${version}`)
-  console.log("✔  Updating hdk and holo_hash rev in Cargo.toml...")
+  console.log('✔  Updating hdk and holo_hash rev in Cargo.toml...')
 
-  const cargoTomlPaths = glob.sync(join(happ_dir, "**", "Cargo.toml"))
+  const cargoTomlPaths = glob.sync(join(happ_dir, '**', 'Cargo.toml'))
   for (const cargoTomlPath of cargoTomlPaths) {
     replace(cargoTomlPath, /^hdk\s*=\s*".+$/m, `hdk = "${crateVersion}"`)
     replace(cargoTomlPath, /^holochain\s*=\s*".+$/m, `holochain = "${crateVersion}"`)
-    replace(cargoTomlPath, /^(?<pre>holochain\s*=\s*{.*version\s*=\s*)".+?"/m, `$<pre>"${crateVersion}"`)
+    replace(
+      cargoTomlPath,
+      /^(?<pre>holochain\s*=\s*{.*version\s*=\s*)".+?"/m,
+      `$<pre>"${crateVersion}"`
+    )
   }
 
-  console.log("✔  Replacing HC version in default.nix...")
+  console.log('✔  Replacing HC version in default.nix...')
   const defaultNixPath = join(happ_dir, 'default.nix')
-  replace(defaultNixPath,
+  replace(
+    defaultNixPath,
     /(?<pre>holochainVersion\s*=\s*{\s*rev\s*=\s*)".+?"/m,
-    `$<pre>"${gitVersion}"`)
+    `$<pre>"${gitVersion}"`
+  )
 
-  console.log("✔  Replacing sha256...")
-  const sha256 = run(`nix-prefetch-url --unpack "https://github.com/holochain/holochain/archive/${gitVersion}.tar.gz"`)
+  console.log('✔  Replacing sha256...')
+  const sha256 = run(
+    `nix-prefetch-url --unpack "https://github.com/holochain/holochain/archive/${gitVersion}.tar.gz"`
+  )
   console.log({ sha256 })
-  replace(defaultNixPath,
+  replace(
+    defaultNixPath,
     /(?<pre>holochainVersion\s*=\s*{[^{]*\n\s*sha256\s*=\s*)".+?"/ms,
-    `$<pre>"${sha256}"`)
+    `$<pre>"${sha256}"`
+  )
 
   // update - nix - by - failure:
 
-  console.log("➳  Corrupting cargoSha256...")
+  console.log('➳  Corrupting cargoSha256...')
 
-  replace(defaultNixPath,
+  replace(
+    defaultNixPath,
     /(?<pre>holochainVersion\s*=\s*{[^{]*\n\s*cargoSha256\s*=\s*)".+?"/ms,
-    `$<pre>"000000000000000000000000000000000000000000000000000a"`)
+    `$<pre>"000000000000000000000000000000000000000000000000000a"`
+  )
 
-  const nixLogPath = join(__dirname, "nix.log")  // TODO this should be a tempdir, in case there are multiple runs simultaneously
+  const nixLogPath = join(__dirname, 'nix.log') // TODO this should be a tempdir, in case there are multiple runs simultaneously
 
-  console.log("➳  Getting cargoSha256... This may take a looooooong time...")
+  console.log('➳  Getting cargoSha256... This may take a looooooong time...')
   run(`nix-shell &> ${nixLogPath}`, { relaxed: true, cwd: happ_dir })
 
   // update - hc - cargoSha:
 
-  console.log("✔  Replacing cargoSha256...")
-  const pattern = /wanted:\s*sha256:000000000000000000000000000000000000000000000000000a\s*got:\s*sha256:(?<cargoSha256>.+?)\b/
-  let nix_log = readFileSync(nixLogPath, 'utf8');
+  console.log('✔  Replacing cargoSha256...')
+  const pattern =
+    /wanted:\s*sha256:000000000000000000000000000000000000000000000000000a\s*got:\s*sha256:(?<cargoSha256>.+?)\b/
+  let nix_log = readFileSync(nixLogPath, 'utf8')
   const match = pattern.exec(nix_log)
   // console.log({ match })
   // console.log({ 'match?.groups': match?.groups })
@@ -79,14 +91,16 @@ function main() {
   const cargoSha256 = match?.groups?.cargoSha256
 
   if (cargoSha256) {
-    replace(defaultNixPath,
+    replace(
+      defaultNixPath,
       /(?<pre>holochainVersion\s*=\s*{[^{]*\n\s*cargoSha256\s*=\s*)".+?"/ms,
-      `$<pre>"${cargoSha256}"`)
+      `$<pre>"${cargoSha256}"`
+    )
   }
 }
 
 function replace(path: string, target: RegExp, replacement: string) {
-  const fileText = readFileSync(path, 'utf8');
+  const fileText = readFileSync(path, 'utf8')
   const newFileText = fileText.replace(target, replacement)
   // console.log(path)
   // console.log(newFileText)
