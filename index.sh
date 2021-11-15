62affa9ad60fe2fe86591b3cb29dda9a0965a112
Harlan T Wood <code@harlantwood.net>
2021-11-14 16:09:03 -1000

    Shell script for bin

--- index.sh
+++ index.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+current_dir=$(pwd)
 which node
-node --loader ts-node/esm --experimental-specifier-resolution=node index.ts $*
+node --loader ${current_dir}/node_modules/ts-node/esm --experimental-specifier-resolution=node index.ts $*
